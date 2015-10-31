26966ac73e0abfc1f9724d44f5ee03c728062165
Hoang Do <hoangdo.dh94@gmail.com>
2015-10-31 20:14:30 +0700

    done

---
+++ js/config.js
@@ -0,0 +1,21 @@
+var PApp = {
+	appId: 'hecRBvHLn26dXkkLfUGxPIWUrbv7uJBJ8JVwwPs3',
+	jsKey: 'QJzUYV2rrwffyIkbyamrfr7jDUwcZmPnEUVGEAEp'
+
+}
+
+var QBApp = {
+  appId: 23236,
+  authKey: 'KSJwvpMhCq7guaS',
+  authSecret: '3LwRgFTdrBhwutG'
+};
+
+var QBUser = {
+	id: 6534809,
+	name: 'Message Deliverer',
+	login: 'message_deliverer',   
+	password: '123123123',
+};
+
+Parse.initialize(PApp.appId, PApp.jsKey);
+QB.init(QBApp.appId, QBApp.authKey, QBApp.authSecret, true);
---
+++ js/main.js
@@ -0,0 +1,104 @@
+var loadingIcon = '<span class="glyphicon glyphicon-repeat spinning"></span>';
+var errorIcon = '<span class="glyphicon glyphicon-remove"></span>';
+var successIcon = '<span class="glyphicon glyphicon-ok"></span>';
+
+disableSubmitButton();
+setNotificationMessage(loadingIcon, 'Initializing...');
+
+QB.createSession({
+    login: QBUser.login,
+    password: QBUser.password
+}, function(err, res) {
+    if (res) {
+        QB.chat.connect({
+            userId: QBUser.id,
+            password: QBUser.password
+        }, function(err, roster) {
+            if (err) {
+                console.log(err);
+                setNotificationMessage(errorIcon, 'Error occurred.');
+            } else {
+                enableSubmitButton();
+                setNotificationMessage(successIcon, 'Ready.');
+
+                $('#message-form').submit(function(e) {
+                    e.preventDefault();
+                    var to = $('#message-form input[type="radio"]:checked').val();
+                    var message = $('#message-form textarea').val();
+
+                    sendMessage(to, message);
+                });
+            }
+        });
+    } else {
+        console.log(err);
+        setNotificationMessage(errorIcon, 'Error occurred.');
+    }
+});
+
+function enableSubmitButton() {
+    $('#message-form button[type="submit"]').removeClass('disabled').prop('disabled', false);
+}
+
+function disableSubmitButton() {
+    $('#message-form button[type="submit"]').addClass('disabled').prop('disabled', true);
+}
+
+function setNotificationMessage(icon, text) {
+    $('#notification').html(icon + ' ' + text);
+}
+
+function sendMessage(to, message) {
+    disableSubmitButton();
+    setNotificationMessage(loadingIcon, 'Sending...');
+    console.log(to, message);
+    getUserChatIds(to, function(ids) {
+        console.log(ids.length, 'users');
+        var msg = {
+            type: 'chat',
+            body: message,
+            extension: {
+                save_to_history: 1,
+            }
+        };
+        ids.forEach(function(id) {
+            QB.chat.send(id, msg);
+        });
+        $('#message-form textarea').val('');
+        enableSubmitButton();
+        setNotificationMessage(successIcon, 'Done!');
+    })
+}
+
+function getUserChatIds(role, callback) {
+    var query = new Parse.Query(Parse.Object.extend('User'));
+    if (role === 'tutor') {
+        console.log('find tutors');
+        query.equalTo('role', 2);
+    } else if (role === 'student') {
+        console.log('find students');
+        query.equalTo('role', 1);
+    } else {
+        console.log('find all');
+    }
+    query.find({
+        success: function(results) {
+            var ids = [];
+            results.forEach(function(result) {
+                var chatId = result.get('chat_user_id');
+                if (chatId) {
+                    ids.push(chatId);
+                }
+            });
+            if (callback) {
+                callback(ids);
+            }
+        },
+        error: function(error) {
+            console.log("Error: " + error.code + " " + error.message);
+            $('#message-form textarea').val('');
+            enableSubmitButton();
+            setNotificationMessage(errorIcon, 'Error occurred. Please try again!');
+        }
+    });
+}
